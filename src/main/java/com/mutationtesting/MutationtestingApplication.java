0a5c738d866c3c849bebf0830b7fff4bb7bb6b18
Simon Low <simon.joseph.low@gmail.com>
2024-04-26 15:42:58 -0400

    Add files via upload

---
+++ src/main/java/com/mutationtesting/MutationtestingApplication.java
@@ -0,0 +1,13 @@
+package com.mutationtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutationtestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutationtestingApplication.class, args);
+	}
+
+}
